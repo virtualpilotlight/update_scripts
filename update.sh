71906340b17fe9da6ae9e5e0d0a57369ffd0031a
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2023-03-21 10:44:46 -0700

    Create update.sh
    wrote this to auto update my Linux systems. Shall add them to crontab to assist in virtualization administration and upkeep.

---
+++ update.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt full-upgrade -y
+
+# above this line tests and works 
+# possible need to add for the kali GUI issue. 
+# xfwm4
